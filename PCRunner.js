9090492aa98a950497235b2ac51a2562ce8a48f6
VanHaoChuan <fanhaochuan0@gmail.com>
2018-07-07 06:07:44 +0800

    Initial commit

---
+++ PCRunner.js
@@ -0,0 +1,11 @@
+/*
+Only use it to test your ModPE
+*/
+// Ids.
+const Podzol = 243;
+const TNT = 46;
+const 下界反应堆 = 247;
+
+function print(context){
+    alert(context);
+}
---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+在LocalCode.js中放置代码.
+千万不要动PCRunner.js中的东西.
