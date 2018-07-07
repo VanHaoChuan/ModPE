3f12ee992aee308db6eae242a030692863d36015
VanHaoChuan <fanhaochuan0@gmail.com>
2018-07-07 09:35:06 +0800

    Copyright + methods

--- PCRunner.js
+++ PCRunner.js
@@ -1,36 +1,13 @@
 /*
 Only use it to test your ModPE
-*/
-//Minecraft
-class modPE{
+idea form jside https://tieba.baidu.com/f?kw=jside&ie=utf-8
 
-    getMinecraftVersion() {
-        return "1.2alpha";
-    }
-     leaveGame() {
-        window.close();
-    }
-     log(Log){
-        console.log(Log);
-        alert("已在控制台打印");
-    }
-     setFoodItem(Id,材质位置,排列值,补充饥饿值,名字,堆叠数量) {
-        alert("已创建"+"Id"+Id+"材质位置"+材质位置+"排列值"+排列值+"补充饥饿值"+补充饥饿值+"名字"+名字+"堆叠数量"+堆叠数量);
-    }
-    setGameSpeed(帧数){
-        alert("已限制帧数"+帧数);
-    }
-    setItem(Id,材质位置,排列值,名字,堆叠数量){
-        alert("已创建"+"Id"+Id+"材质位置"+材质位置+"排列值"+排列值+"名字"+名字+"堆叠数量"+堆叠数量);
-    }
-    takeScreenshot(路径){
-        alert("已截图");
-    }
-    showTipMessage(文字){
-        document.writeln(文字)
-    }
-}
-var ModPE = new modPE();
+
+thanks for 初始状态0.
+
+
+Copyright (c) 2018 wdsjhehe在路上 All rights reserved.
+*/
 //Package control.
 class things{
     constructor(Id){
@@ -63,7 +40,7 @@ class Ent {
 }
 //Player.
 class Player{
-constructor(Id,Health,Hunger,X,Y,Z,Riding){
+constructor(Id,Health,Hunger,X,Y,Z,Riding,CanFly){
     this.id = Id;
     this.health = Health;
     this.hunger = Hunger;
@@ -71,10 +48,29 @@ constructor(Id,Health,Hunger,X,Y,Z,Riding){
     this.y = Y;
     this.z = Z;
     this.riding = Riding;
+    this.canFly = CanFly;
+
+}
+
+//functions
+canFly(){
+    return this.canFly;
+}
+isFly(){
+return false;
+}
+setCanFly(可否){
+this.canFly = 可否;
+}
+getPointedBlockId(){
+return "玩家所指方块ID";
+}
+getPointedBlockSide(){
+return "玩家所指方块面";
 }
 
 }
-var player = new Player(63,20,20,0,0,0,new Ent(null));
+var player = new Player(63,20,20,0,0,0,new Ent(null),false);
 //Ids.
 const Podzol = 243;
 const TNT = 46;
@@ -143,4 +139,43 @@ function setVelY(实体,距离_格) {
 }
 function setVelZ(实体,距离_格) {
     alert ("已将实体沿Z传送"+距离_格+"格");
+}
+//Minecraft
+class modPE{
+
+    getMinecraftVersion() {
+        return "1.2alpha";
+    }
+     leaveGame() {
+        window.close();
+    }
+     log(Log){
+        console.log(Log);
+        alert("已在控制台打印");
+    }
+     setFoodItem(Id,材质位置,排列值,补充饥饿值,名字,堆叠数量) {
+        alert("已创建"+"Id"+Id+"材质位置"+材质位置+"排列值"+排列值+"补充饥饿值"+补充饥饿值+"名字"+名字+"堆叠数量"+堆叠数量);
+    }
+    setGameSpeed(帧数){
+        alert("已限制帧数"+帧数);
+    }
+    setItem(Id,材质位置,排列值,名字,堆叠数量){
+        alert("已创建"+"Id"+Id+"材质位置"+材质位置+"排列值"+排列值+"名字"+名字+"堆叠数量"+堆叠数量);
+    }
+    takeScreenshot(路径){
+        alert("已截图");
+    }
+    showTipMessage(文字){
+        document.writeln(文字);
+    }
+}
+var ModPE = new modPE();
+//Level functions.
+class level{
+    setGrass(X,Y,颜色_字符串_16进制){
+            alert("已将位于"+X+","+Y+"的草涂成"+颜色_字符串_16进制);
+    }
+
+
+
 }
